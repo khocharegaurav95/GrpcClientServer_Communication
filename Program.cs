2c4aa827cfa6cf7403e086a4342a1e40c95a139d
Gaurav Khochare <khocharegaurav85@gmail.com>
2023-07-30 23:25:38 +0530

    Add project files.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
-﻿using Grpc.Net.Client;
+﻿using GrpcServer.Services;
+using Grpc.Net.Client;
 using System;
 using System.Net;
 
+var builder = WebApplication.CreateBuilder(args);
 namespace GrpcClient
 {
     class Program
@@ -11,16 +13,25 @@ namespace GrpcClient
             ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
             using var channel= GrpcChannel.ForAddress("https://localhost:5001");
 
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
             var input = new HelloRequest { Name = "Greeter Client" };
             var client= new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(input);
-            
-            Console.WriteLine($"Greetings:{reply.Message}");
-            
 
+// Add services to the container.
+builder.Services.AddGrpc();
+            Console.WriteLine($"Greetings:{reply.Message}");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
             Console.WriteLine("Press Any Key to Exit...");
             Console.ReadLine();
         }
     }
 }
 
+app.Run();
